2a4a3a56d00bf56644adebe3c38b4a0dc42d627b
Roberto <1135584@atitus.edu.br>
2024-06-27 14:34:56 -0300

    Ultimo

--- src/main/java/br/edu/atitus/denguealerta/configs/ConfigSecurity.java
+++ src/main/java/br/edu/atitus/denguealerta/configs/ConfigSecurity.java
@@ -2,40 +2,43 @@ package br.edu.atitus.denguealerta.configs;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import br.edu.atitus.denguealerta.components.AuthToken;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-
 public class ConfigSecurity {
+
 	@Bean
 	WebMvcConfigurer corsConfigurer() {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/*").allowedOrigins("");
+				registry.addMapping("/**")
+						.allowedOrigins("*")
+						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+						.allowedHeaders("*")
+						.allowCredentials(false)
+						.maxAge(3600);
 			}
 		};
 	}
-	
+
 	@Bean
 	SecurityFilterChain getSecurity(HttpSecurity http, AuthToken authToken) throws Exception {
 		http
-			.csrf(csrf -> csrf.disable())
-			.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-			.authorizeHttpRequests(auth -> auth
-					.requestMatchers("/ws**", "/ws/**").authenticated()
-					.anyRequest().permitAll()
-					)
-			.addFilterBefore(authToken, UsernamePasswordAuthenticationFilter.class);
+				.csrf(csrf -> csrf.disable())
+				.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+				.authorizeHttpRequests(auth -> auth
+						.requestMatchers("/ws**", "/ws/**").authenticated()
+						.anyRequest().permitAll()
+				)
+				.addFilterBefore(authToken, UsernamePasswordAuthenticationFilter.class);
 		return http.build();
 	}
-
 }
