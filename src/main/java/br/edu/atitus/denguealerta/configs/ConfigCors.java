c16ab0283081a1098220147ff3f33adb594814cb
Roberto <1135584@atitus.edu.br>
2024-06-27 12:13:57 -0300

    Cors

--- src/main/java/br/edu/atitus/denguealerta/configs/ConfigCors.java
+++ src/main/java/br/edu/atitus/denguealerta/configs/ConfigCors.java
@@ -10,7 +10,7 @@ public class ConfigCors implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("*")
+        registry.addMapping("/*")
             .allowedOrigins("*")
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
             .allowedHeaders("*")
